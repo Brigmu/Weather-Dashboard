543d8f8d6b6afe99e68dd8cdf48895a9c8028637
Brigham Mueller <brigmu@gmail.com>
2020-01-22 14:48:32 -0800

    added functionality to search a specific city. added local storage saving for searched cities

--- script.js
+++ script.js
@@ -1,38 +1,110 @@
 const cityInputEl = document.querySelector('#city-input');
 const submitBtnEl = document.querySelector('#city-submit');
 const dashboardEl = document.querySelector('.city-forecast');
+const savedCitiesEl = document.querySelector('.saved-cities');
 
 
-const url = 'https://api.openweathermap.org/data/2.5/forecast?q=';
-let city = 'Seattle';
+const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+let city = '';
 const auth = "&appid=fbe457a4568f0bd33f8aea7d30a321e7"
 const dataType = '&units=imperial'
-const fullUrl = url + city + auth + dataType;
+let citiesArr = JSON.parse(window.localStorage.getItem('cities'));
+if (citiesArr == null) {
+    citiesArr = [];
+}
+renderCities();
 
-fetch(fullUrl)
-    .then(function(response){
-        return response.json();
-    })
-        .then(function(json){
+function getWeatherData(fullWeatherUrl, fullForecastUrl){
+    fetch(fullWeatherUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
             console.log(json);
             populateTodaysForecast(json);
         });
 
+    fetch(fullForecastUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            console.log(json);
+            populate5DayForecast(json);
+        });
+
+}
+
+submitBtnEl.addEventListener('click', function(event){
+    event.preventDefault();
+
+    let userCityName = cityInputEl.value.trim();
+    city = userCityName;
+    console.log(city);
+    const fullForecastUrl = forecastUrl + city + auth + dataType;
+    const fullWeatherUrl = weatherUrl + city + auth + dataType;
+    citiesArr.push(userCityName);
+
+    window.localStorage.setItem('cities', JSON.stringify(citiesArr));
+
+    getWeatherData(fullWeatherUrl,fullForecastUrl);
+    renderCities();
+
+})
+
+function renderCities() {
+    let citiesArr = JSON.parse(window.localStorage.getItem('cities'));
+    if (citiesArr == null) {
+        citiesArr = [];
+    }
+    savedCitiesEl.innerHTML = '';
+
+    for (let i = 0; i < citiesArr.length; i++) {
+        let newBtn = document.createElement('button');
+        newBtn.setAttribute('class', 'city');
+        newBtn.setAttribute('data-name', citiesArr[i]);
+        newBtn.textContent = citiesArr[i];
+        savedCitiesEl.append(newBtn);
+    }
+}
+
+
 function populateTodaysForecast(obj) {
     const cityName = document.createElement('h2');
-    cityName.textContent = obj.city.name;
+    cityName.textContent = obj.name;
     dashboardEl.append(cityName);
     
     const cityTemp = document.createElement('p');
-    cityTemp.textContent = `Temperature: ${obj.list[0].main.temp} °F`;
+    cityTemp.textContent = `Temperature: ${obj.main.temp} °F`;
     dashboardEl.append(cityTemp);
 
     const cityHumidity = document.createElement('p');
-    cityHumidity.textContent = `Humidity: ${obj.list[0].main.humidity}%`;
+    cityHumidity.textContent = `Humidity: ${obj.main.humidity}%`;
     dashboardEl.append(cityHumidity);
 
     const cityWindSpeed = document.createElement('p');
-    cityWindSpeed.textContent = `Wind Speed: ${obj.list[0].wind.speed} MPH`;
+    cityWindSpeed.textContent = `Wind Speed: ${obj.wind.speed} MPH`;
     dashboardEl.append(cityWindSpeed);
 
+}
+
+function populate5DayForecast(obj) {
+    for (let i = 0; i < 5; i++) {
+        let j  = i * 8;
+        let k = i + 1;
+        console.log(j);
+        const dayEl = document.querySelector(`#day-${k}`);
+        const cityTemp = document.createElement('p');
+        cityTemp.textContent = `Temperature: ${obj.list[j].main.temp} °F`;
+        dayEl.append(cityTemp);
+
+        const cityHumidity = document.createElement('p');
+        cityHumidity.textContent = `Humidity: ${obj.list[j].main.humidity}%`;
+        dayEl.append(cityHumidity);
+
+        const cityWindSpeed = document.createElement('p');
+        cityWindSpeed.textContent = `Wind Speed: ${obj.list[j].wind.speed} MPH`;
+        dayEl.append(cityWindSpeed);
+    }
 }
